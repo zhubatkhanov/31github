89c3b6c8978e592540c51032ded278b775a8bf3b
zhubatkhanov <zhubatkhanovsanzhar@gmail.com>
2023-12-01 17:02:21 +0600

    278th task

---
+++ binary_search/278.First_Bad_Version/main.py
@@ -0,0 +1,35 @@
+"""
+You are a product manager and currently leading a team to develop a new product.
+Unfortunately, the latest version of your product fails the quality check.
+Since each version is developed based on the previous version,
+all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one,
+which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which returns whether version is bad.
+Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+Example 1:
+
+Input: n = 5, bad = 4
+Output: 4
+Explanation:
+call isBadVersion(3) -> false
+call isBadVersion(5) -> true
+call isBadVersion(4) -> true
+Then 4 is the first bad version.
+"""
+
+def firstBadVersion(self, n):
+    left, right = 1, n
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if isBadVersion(mid) == False:
+            left = mid + 1
+        elif isBadVersion(mid) == True:
+            if isBadVersion(mid - 1) == False:
+                return mid
+            right = mid
