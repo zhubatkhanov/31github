37d8d015d0e3131068f0a2e4dcc8e68d07c570dd
zhubatkhanov <zhubatkhanovsanzhar@gmail.com>
2023-12-05 11:59:03 +0600

    add 27th task:

---
+++ two_pointers/27.Remove_Element/main.py
@@ -0,0 +1,35 @@
+"""
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+
+
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+"""
+
+def removeElement(nums, val):
+    slow = fast = 0
+
+    while fast < len(nums):
+        if nums[fast] != val:
+            nums[slow] = nums[fast]
+        else:
+            while nums[fast] == val:
+                fast += 1
+                if fast == len(nums):
+                    return slow
+
+            nums[slow] = nums[fast]
+
+        slow += 1
+        fast += 1
+    return slow
+
+
+print(removeElement([3,3], 3))
