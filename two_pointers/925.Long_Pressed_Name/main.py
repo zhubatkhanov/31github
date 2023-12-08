1fa6eb2a3817105f2f9c33f8992b9f6f1b755b82
zhubatkhanov <zhubatkhanovsanzhar@gmail.com>
2023-12-08 21:28:04 +0500

    add optimize solution from the internet

--- two_pointers/925.Long_Pressed_Name/main.py
+++ two_pointers/925.Long_Pressed_Name/main.py
@@ -13,6 +13,20 @@ Input: name = "alex", typed = "aaleex"
 Output: true
 Explanation: 'a' and 'e' in 'alex' were long pressed.
 """
+# optimize solution
+# def isLongPressedName(name, typed):
+#     i, j = 0, 0
+#
+#     while j < len(typed):
+#         if i < len(name) and name[i] == typed[j]:
+#             i += 1
+#             j += 1
+#         elif j > 0 and typed[j] == typed[j - 1]:
+#             j += 1
+#         else:
+#             return False
+#
+#     return i == len(name)
 
 
 # my solution :)
