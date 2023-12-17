367a30441a8b25153fd1dfb6e506e005e6958e65
zhubatkhanov <zhubatkhanovsanzhar@gmail.com>
2023-12-17 18:40:53 +0500

    add second solution

--- top_interview_150/array_string/roman_to_integer(13).py
+++ top_interview_150/array_string/roman_to_integer(13).py
@@ -52,3 +52,22 @@ def romanToInt(s):
 
 s = "MCMXCIV"
 print(romanToInt(s))
+
+## 2nd solution (LeetCode)
+def romanToInt(s):
+    translations = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+    number = 0
+    s = s.replace("IV", "IIII").replace("IX", "VIIII")
+    s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+    s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+    for char in s:
+        number += translations[char]
+    return number
